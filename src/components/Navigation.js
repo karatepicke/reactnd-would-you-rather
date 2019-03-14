535e9d189d40cbef929971a5e00d9db029e3ac03
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-14 11:12:23 +0100

    feat: Replaces Link with NavLink

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,56 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
 export default class WYRNavbar extends React.Component {
-  state = { 
-    activeItem: this.props.active
-  }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
   render() {
-    const { activeItem } = this.state
-
     return (
       <div>
         <Menu pointing secondary>
-          <Link to="/">
-            <Menu.Item 
-              onClick={this.handleItemClick}
-              name='home' 
-              active={activeItem === 'home'} 
-            />
-          </Link>
-          <Link to="/questions">
-            <Menu.Item
-              onClick={this.handleItemClick}
-              name='questions'
-              active={activeItem === 'questions'}
-            />
-          </Link>
-          <Link to="/answers">
-            <Menu.Item
-              onClick={this.handleItemClick}
-              name='answers'
-              active={activeItem === 'answers'}
-            />
-          </Link>
-          <Link to="/leaderboard">
-            <Menu.Item
-              onClick={this.handleItemClick}
-              name='leaderboard'
-              active={activeItem === 'leaderboard'}
-            />
-          </Link>
+          <NavLink
+            exact={true}
+            to="/"
+            className="item"
+            name='home'
+            activeClassName="active"
+          >Home</NavLink>
+          <NavLink to="/questions"
+            className="item"
+            name='questions'
+            activeClassName="active"
+          >Questions</NavLink>
+          <NavLink to="/answers"
+            className="item"
+            name='answers'
+            activeClassName="active"
+          >Answers </NavLink>
+          <NavLink to="/leaderboard"
+            className="item"
+            name='leaderboard'
+            activeClassName="active"
+          >Leaderboard</NavLink>
           <Menu.Menu position='right'>
-            <Link to="/login"> 
-              <Menu.Item
-                onClick={this.handleItemClick}
-                name='login'
-                active={activeItem === 'login'}
-              />
-            </Link>
+            <NavLink
+              to="/login"
+              className="item"
+              onClick={this.handleItemClick}
+              name='login'
+              activeClassName="active"
+            >Login</NavLink>
           </Menu.Menu>
         </Menu>
       </div>
