7110151bb6bf8f3c1a6631d8c08ab06e599dc2f6
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-14 11:16:21 +0100

    refactor: Removes unnecessary components from render

--- src/components/SignInUp/SignInUpPrompt.js
+++ src/components/SignInUp/SignInUpPrompt.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Divider, Grid, Header, Icon, Segment } from 'semantic-ui-react';
+import { Grid, Header, Icon, Segment } from 'semantic-ui-react';
 import SignInUpForm from './SignInUpForm';
 
 const SignInUpPrompt = () => (
   <Segment placeholder>
-    <Grid columns={2} stackable textAlign='center'>
-      <Divider vertical>Or</Divider>
-
+    <Grid columns={1} stackable textAlign='center'>
       <Grid.Row verticalAlign='middle'>
         <Grid.Column>
           <Header icon>
@@ -16,16 +13,6 @@ const SignInUpPrompt = () => (
           </Header>
 
           <SignInUpForm />
-        </Grid.Column>
-
-        <Grid.Column>
-          <Header icon>
-            <Icon name='add user' />
-            Create Account
-          </Header>
-          <Link to="/register">
-            <Button primary>Create</Button>
-          </Link>
         </Grid.Column>
       </Grid.Row>
     </Grid>
