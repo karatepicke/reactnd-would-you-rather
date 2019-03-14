e72ad3d9a0d91939b2518621997791b4d278b39e
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-14 11:10:36 +0100

    feat: Moves Router from App.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
-import { combineReducers } from './store/reducers/rootReducer';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { rootReducer } from './store/reducers/rootReducer';
+import { Route } from 'react-router';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(combineReducers);
+// Pages
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import QuestionsPage from './pages/QuestionsPage';
+import AnswersPage from './pages/AnswersPage';
+import LeaderboardPage from './pages/LeaderboardPage';
+
+// Styles
+import './index.css';
+import './App.css';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/questions" component={QuestionsPage} />
+        <Route path="/answers" component={AnswersPage} />
+        <Route path="/leaderboard" component={LeaderboardPage} />
+      </div>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
