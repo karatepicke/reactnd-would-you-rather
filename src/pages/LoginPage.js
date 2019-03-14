0a13f2a1de3e20d8e14ff4650591b123dc925084
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-14 14:42:15 +0100

    feat: Adds redirect to home on login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import WYRNavbar from '../components/Navigation';
 import { Container } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import SignInUpPrompt from '../components/SignInUp/SignInUpPrompt';
 
 class LoginPage extends React.Component {
-  componentDidMount() {
-    this.props.authedUser && this.props.history.push('/home')
+  componentWillReceiveProps(props) {
+    if (props.user) {
+      props.history.push('/')
+    }
   }
 
   render() {
@@ -20,4 +23,5 @@ class LoginPage extends React.Component {
   }
 }
 
-export default LoginPage;
+const mapStateToProps = ({ authedUser }) => ({ user: authedUser.user })
+export default connect(mapStateToProps)(LoginPage);
