6fdab1f8577c842c51c379592de191669157393c
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-15 10:27:28 +0100

    feat: Adds questions to user-actions

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,23 +1,24 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
 export const DESTROY_AUTHED_USER = 'DESTROY_AUTHED_USER';
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER';
+export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
+export const GET_UNSANSWERED_QUESTIONS_FOR_SIGNED_IN_USER = 'GET_UNSANSWERED_QUESTIONS_FOR_SIGNED_IN_USER'
 
-export function setuser(user) {
+export function setUser(user) {
   return {
     type: SET_AUTHED_USER,
     user,
   }
 }
 
-export function destroyuser() {
+export function destroyUser() {
   return {
     type: DESTROY_AUTHED_USER,
     user: undefined
   }
 }
 
-export const RECEIVE_USERS = 'RECEIVE_USERS';
-export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER';
-export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
 
 export function receiveUsers(users) {
   return {
@@ -26,30 +27,39 @@ export function receiveUsers(users) {
   }
 }
 
-function addQuestionToUser(question) {
+export function getUnansweredQuestionsForSignedInUser(questions) {
   return {
-    type: ADD_QUESTION_TO_USER,
-    question,
+    type: GET_UNSANSWERED_QUESTIONS_FOR_SIGNED_IN_USER,
+    payload: {
+      questions,
+    }
   }
 }
 
-export function handleAddQuestionToUser(info) {
-  return (dispatch) => {
-    dispatch(addQuestionToUser(info))
-  }
-}
+// function addQuestionToUser(question) {
+//   return {
+//     type: ADD_QUESTION_TO_USER,
+//     question,
+//   }
+// }
 
-function addAnswerToUser(user, qid, answer) {
-  return {
-    type: ADD_ANSWER_TO_USER,
-    user,
-    qid,
-    answer,
-  }
-}
+// export function handleAddQuestionToUser(info) {
+//   return (dispatch) => {
+//     dispatch(addQuestionToUser(info))
+//   }
+// }
 
-export function handleAddAnswerToUser(user, qid, answer) {
-  return (dispatch) => {
-    dispatch(addAnswerToUser(user, qid, answer))
-  }
-}
+// function addAnswerToUser(user, qid, answer) {
+//   return {
+//     type: ADD_ANSWER_TO_USER,
+//     user,
+//     qid,
+//     answer,
+//   }
+// }
+
+// export function handleAddAnswerToUser(user, qid, answer) {
+//   return (dispatch) => {
+//     dispatch(addAnswerToUser(user, qid, answer))
+//   }
+// }
