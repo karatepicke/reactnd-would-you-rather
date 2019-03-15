4d7498bf008702c99dc2229a221d3d5b194eae3e
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-15 10:26:20 +0100

    refactor: Removes questions fron rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './user';
-import questions from './questions';
 
 export const rootReducer = combineReducers({
   user,
-  questions,
 });
