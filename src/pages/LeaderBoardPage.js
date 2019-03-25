bbd7c71394452fbe035deab6fdbeb2a4248eea15
Dominik Pickenäcker <pickenecker.dominik@gmail.com>
2019-03-25 21:31:00 +0100

    feat: First draft of LeaderboardPage

--- src/pages/LeaderBoardPage.js
+++ src/pages/LeaderBoardPage.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+// custom components
 import WYRNavbar from '../components/Navigation';
 
 class LeaderboardPage extends React.Component {
+  componentWillMount() {
+    if (!this.props.user) {
+      this.props.history.push('/login')
+      return
+    }
+
+    // this.getAuthorAvatarUrl();
+  }
+
   render() {
     return(
       <div className="leaderboard">
         <WYRNavbar active="leaderboard" />
-        LEADERBOARD
+        
       </div>
     )
   }
 }
 
-export default LeaderboardPage;
+const mapStateToProps = ({user}) => ({
+  user: user.user
+})
+export default connect(mapStateToProps)(LeaderboardPage);
